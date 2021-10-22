79a0983b6f2ea4f5efb938ef0a44698a610a9778
code-hero-projects <code.hero.email@gmail.com>
2021-10-22 18:34:23 +0100

    frontend: changed App to fetch state from reducer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
-import { HomePage } from "./pages";
+import { useSelector } from "react-redux";
+import { CurrentGameState } from "./models";
+import { EndGamePage, HomePage, InGamePage } from "./pages";
+import { selectGameState } from "./redux";
 
 function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+  const { gameState } = useSelector(selectGameState);
+
+  const gamePages = {
+    [CurrentGameState.NotStarted]: <HomePage />,
+    [CurrentGameState.Playing]: <InGamePage />,
+    [CurrentGameState.Ended]: <EndGamePage />
+  };
+
+  return gamePages[gameState];
 }
 
 export default App;
