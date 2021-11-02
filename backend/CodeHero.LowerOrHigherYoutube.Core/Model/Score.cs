a4faff3eacaa9aa440932b83ae87a88a36884d75
code-hero-projects <code.hero.email@gmail.com>
2021-11-02 18:53:30 +0000

    backend: added Score model

---
+++ backend/CodeHero.LowerOrHigherYoutube.Core/Model/Score.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeHero.LowerOrHigherYouTube.Core.Model
+{
+    public class Score
+    {
+        public int Id { get; set; }
+        public int Points { get; set; }
+        public string PlayerName { get; set; }
+        public string Country { get; set; }
+    }
+}
