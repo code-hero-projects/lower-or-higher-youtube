23cfab9f2b231211fce828415532f456ec7f317e
code-hero-projects <code.hero.email@gmail.com>
2022-05-26 22:51:32 +0100

    changed cache behavior

--- frontend/src/redux/video/epics.ts
+++ frontend/src/redux/video/epics.ts
@@ -21,7 +21,7 @@ export function randomizeVideos(videos: Video[]): Video[] {
 export const getVideos = createAsyncThunk<Video[], number>('videos/fetchVideos', async(countryId: number) => {
   const countryVideosFetchedTimestamp = await countryVideosFetchedTimestampRepository.read(countryId);
 
-  if (!countryVideosFetchedTimestamp) {
+  if (!countryVideosFetchedTimestamp || hasCacheExpired(countryVideosFetchedTimestamp)) {
     const videos = await shuffleVideos(CodeHeroApi.getVideos(countryId));
 
     await insertFetchedTimestamp(countryId);
@@ -41,9 +41,18 @@ async function shuffleVideos(videosPromise: Promise<Video[]>): Promise<Video[]> 
 async function insertFetchedTimestamp(countryId: number) {
   const currentDate = new Date();
   const fetchedTimestamp: FetchedTimestamp = { id: countryId, date: currentDate };
+  await countryVideosFetchedTimestampRepository.delete(fetchedTimestamp.id);
   await countryVideosFetchedTimestampRepository.insert(fetchedTimestamp);
 }
 
 async function insertVideos(videos: Video[]) {
-  videos.forEach(async video => await videoRepository.insert(video));
+  videos.forEach(async video => {
+    await videoRepository.delete(video.id);
+    await videoRepository.insert(video);
+  });
+}
+
+function hasCacheExpired(countryVideosFetchedTimestamp: FetchedTimestamp): boolean {
+  const currentDate = new Date();
+  return countryVideosFetchedTimestamp.date.getDate() !== currentDate.getDate();
 }
